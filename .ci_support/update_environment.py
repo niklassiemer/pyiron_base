345651002167995c35c0242b539c4d6add42be25
Niklas Siemer <70580458+niklassiemer@users.noreply.github.com>
2021-07-05 21:52:28 +0200

    Update update_environment.py

--- .ci_support/update_environment.py
+++ .ci_support/update_environment.py
@@ -13,12 +13,12 @@ def convert_package_name(name, name_conv_dict):
     return result
 
 
-if not (sys.argv[0] == 'Bump' and sys.argv[2] == 'from' and sys.argv[4] == 'to'):
+if not (sys.argv[1] == 'Bump' and sys.argv[3] == 'from' and sys.argv[5] == 'to'):
     raise ValueError("Title of a dependabot PR expected")
 
-package_to_update = sys.argv[1]
-from_version = sys.argv[3]
-to_version = sys.argv[5]
+package_to_update = sys.argv[2]
+from_version = sys.argv[4]
+to_version = sys.argv[6]
 
 with open('.ci_support/pypi_vs_conda_names.json', 'r') as f:
     name_conversion_dict = json.load(f)
